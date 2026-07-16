f17d964dde0c6d2ad7691656df0e15abe8b4fc16
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-16 15:48:44 +0300

    Use cgride.cpp examples

--- static-library/cgride.cpp
+++ static-library/cgride.cpp
@@ -5,10 +5,10 @@ void cgride_configure(cgride::project::Project &project)
   auto &core = project.static_library("core");
 
   core.sources("core/src/message.cpp");
-  core.include_dirs("core/include");
+  core.public_include_directory("core/include");
 
   auto &app = project.executable("app");
 
   app.sources("app/src/main.cpp");
-  app.links(core);
+  app.link(core);
 }
